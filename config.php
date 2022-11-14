920f66641bde73923d35d48209c8116d77a57cbf
Leandro <leandrovdsantos96@gmail.com>
2022-11-13 22:13:22 -0300

    update

--- config.php
+++ config.php
@@ -1,10 +1,22 @@
 <?php 
-    define('HOST', 'localhost');
-    define('USER', 'root');
-    define('PASS', 'osover14e');
-    define('BASE', 'cadastro');
-
-$conn = new MySQLi(HOST, USER, PASS, BASE);
+    //define('HOST', 'localhost');
+    //define('USER', 'root');
+    //define ('PASS', 'osover14e');
+    //define('BASE', 'cadastro');
 
 
 
+//$conn = new MySQLi(HOST, USER, PASS, BASE);
+
+$HOST = "localhost";
+$BASE = "cadastro";
+$USER = "root";
+$PASS = "osover14e";
+$CRIPT = md5($PASS);
+echo $CRIPT;
+# Cria a conexão
+$conn = mysqli_connect($HOST, $USER, $PASS, $BASE);
+# Verifica a conexão
+
+
+?>
